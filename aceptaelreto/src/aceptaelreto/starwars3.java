47f3a46c7d1683a5b10750273c0cf15ba199ec72
tomaseroles <tomas.eroles@wanadoo.es>
2018-01-18 16:37:08 +0100

    tercer problema de star wars

---
+++ aceptaelreto/src/aceptaelreto/starwars3.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aceptaelreto;
+import java.util.Scanner;
+
+/**
+ *
+ * @author tomfor
+ */
+public class starwars3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
