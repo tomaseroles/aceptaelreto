928cbac0eed0710b756a05b536db5270b8c2dcec
tomaseroles <tomas.eroles@wanadoo.es>
2018-01-18 20:12:13 +0100

    tercer problema de star wars

--- aceptaelreto/src/aceptaelreto/starwars3.java
+++ aceptaelreto/src/aceptaelreto/starwars3.java
@@ -18,29 +18,32 @@ public class starwars3 {
         System.out.println("Problema 3. Organizando los hangares");
         System.out.print("\nHangares: ");
         hangares = sc.nextInt();
-        int hangar[] = new int[hangares];
-        for (int i=0;i<hangares;i++) {
-            hangar[i]=sc.nextInt();
+        while (hangares!=0) {
+            int hangar[] = new int[hangares];
+            for (int i=0;i<hangares;i++) {
+                hangar[i]=sc.nextInt();
+            }
+            System.out.print("\nNaves: ");
+            naves = sc.nextInt();
+            int nave[] = new int[naves];
+            for(int i=0;i<naves;i++){
+                nave[i]=sc.nextInt();
+            }
+
+            if(ResuelveCaso(hangar,nave))
+                System.out.println("SI");
+            else
+                System.out.println("NO");
+            hangares = sc.nextInt();
         }
-        System.out.print("\nNaves: ");
-        naves = sc.nextInt();
-        int nave[] = new int[naves];
-        for(int i=0;i<naves;i++){
-            nave[i]=sc.nextInt();
-        }
-        
-        if(ResuelveCaso(hangar,nave))
-            System.out.println("SI");
-        else
-            System.out.println("NO");
         sc.close();
     }
     
     public static boolean ResuelveCaso(int[] h, int[] n) {
         boolean salida=false;
         for (int i=0;i<n.length;i++){
-            int hangar = qHangarMax(h, n[i]);
-            if (hangar>=0) {
+            int hangar = HangarMayor(h);
+            if (hangar>=n[i]) {
                 h[hangar]=h[hangar]-n[i];
                 salida = true;
             } else
@@ -49,13 +52,17 @@ public class starwars3 {
         return salida;
     }
     
-    public static int qHangarMax(int[] h, int nave) {
-        int salida = -1;
-        int maximo=1000000;
-        for (int i=0;i<h.length;i++) {
-            
+    
+    public static int HangarMayor(int[] hangar){
+        int maximo = -1;
+        int posicion =-1;
+        for (int i=0;i<hangar.length;i++){
+            if (hangar[i]>maximo) {
+                maximo=hangar[i];
+                posicion = i;
+            }
         }
-        return salida;
+        return posicion;
     }
     
 }
