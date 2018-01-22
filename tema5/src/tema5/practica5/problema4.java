e4b2146f208d05bf5434e4def162fcec4a3d4765
tomaseroles <tomas.eroles@wanadoo.es>
2018-01-22 20:32:11 +0100

    tema 5. inicio

---
+++ tema5/src/tema5/practica5/problema4.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema5.practica5;
+import java.util.Scanner;
+
+/**
+ *
+ * @author tomfor
+ */
+public class problema4 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Rayuela.");
+        System.out.print("Numero de valores: ");
+        int n = sc.nextInt();
+        System.out.println("El numero de caminos es: " + CaminosPosibles(n,0));
+    }
+    
+    public static int CaminosPosibles(int max,int i) {
+        
+        
+        
+        return i;
+    }
+}
