5808ac2c9f84c952db5539f4c82460fecc8adef9
tomaseroles <tomas.eroles@wanadoo.es>
2018-02-06 21:02:20 +0100

    invertir en jaen modificado

--- aceptaelreto/src/jaen/olivas.java
+++ aceptaelreto/src/jaen/olivas.java
@@ -15,7 +15,7 @@ public class olivas {
         Scanner sc = new Scanner(System.in);
         //System.out.println("Inviertiendo en Jaen");
         do{
-            int maximo=0;
+            int maximo=0, aux=0;
             int filas = sc.nextInt();
             int columnas = sc.nextInt();
             char[][] campo = new char[filas][columnas];
@@ -24,29 +24,41 @@ public class olivas {
             for(int i=0;i<filas;i++)
                 campo[i]=sc.nextLine().toCharArray();
             //MostrarCampo(campo);
-            System.out.println(Zonas(campo,0,0,maximo));
-            
+            while(HayOlivos(campo)) {
+                aux=Zonas(campo,0,0,maximo);
+                if (aux>maximo)
+                    maximo=aux;
+            }
+            System.out.println(maximo);
         } while(sc.hasNext());
     }
         
     private static int Zonas(char[][] m,int i,int j, int z){
-        int salida=0;
+        System.out.println(i+"\t"+j+"\t"+m[i][j]);
+        
+        if(i<0 || i>=m.length || j<0 || j>=m[i].length)
+            return 0;
+        if(m[i][j]==' ')
+            return 0;
         if(m[i][j]=='#'){        // se ha encontrado un campo. hay que mirar alrededor si tambien hay campos
-            if(m[i][j+1]=='#' && i>=0 && i<=m.length && j+1>=0 && j+1<=m[i].length){
-                m[i][j+1]='*';
-                salida = z+Zonas(m,i,j+1,z);
-            }
-            if(m[i][j-1]=='#' && i>=0 && i<=m.length && j-1>=0 && j-1<=m[i].length) {
-                m[i][j-1]='*';
-                salida = z+Zonas(m,i,j-1,z);
-            }
-            if(m[i+1][j]=='#' && i+1>=0 && i+1<=m.length && j>=0 && j<=m[i].length) {
-                m[i+1][j]='*';
-                salida = z+Zonas(m,i+1,j,z);
-            }
-            if(m[i-1][j]=='#' && i-1>=0 && i-1<=m.length && j>=0 && j<=m[i].length) {
-                salida = m[i-1][j]='*';
-                return z+Zonas(m,i-1,j,z);
+            m[i][j]=' ';
+            z++;
+            z = z+Zonas(m,i,j+1,z);
+            z = z+Zonas(m,i,j-1,z);
+            z = z+Zonas(m,i-1,j,z);
+            z = z+Zonas(m,i+1,j,z);
+            return z;
+        }
+        return 0;
+    }
+    
+    private static boolean HayOlivos(char[][] m){
+        boolean salida=false;
+        
+        for(int i=0;i<m.length;i++){
+            for(int j=0;j<m[i].length;j++){
+                if(m[i][j]=='#')
+                    salida=true;
             }
         }
         return salida;
